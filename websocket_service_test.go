77ba4d309b5f3ebd518fffc673eafa50459e2f6d
shaolinjehzu <maltsev@kitnk.ru>
2020-09-15 14:37:03 +0700

    update

--- websocket_service_test.go
+++ websocket_service_test.go
@@ -170,7 +170,7 @@ func (s *websocketServiceTestSuite) assertWsPartialDepthEventEqual(e, a *WsParti
 	}
 }
 
-func (s *websocketServiceTestSuite) TestDepthServe() {
+func (s *websocketServiceTestSuite) TestDepthFeatureServe() {
 	data := []byte(`{
         "e": "depthUpdate",
         "E": 1499404630606,
@@ -206,7 +206,226 @@ func (s *websocketServiceTestSuite) TestDepthServe() {
 	s.mockWsServe(data, errors.New(fakeErrMsg))
 	defer s.assertWsServe()
 
-	doneC, stopC, err := WsDepthServe("ETHBTC", func(event *WsDepthEvent) {
+	doneC, stopC, err := WsDepthFeatureServe("ETHBTC", func(event *WsDepthEvent) {
+		e := &WsDepthEvent{
+			Event:         "depthUpdate",
+			Time:          1499404630606,
+			Symbol:        "ETHBTC",
+			UpdateID:      7913455,
+			FirstUpdateID: 7913452,
+			Bids: []Bid{
+				{
+					Price:    "0.10376590",
+					Quantity: "59.15767010",
+				},
+			},
+			Asks: []Ask{
+				{
+					Price:    "0.10376586",
+					Quantity: "159.15767010",
+				},
+				{
+					Price:    "0.10383109",
+					Quantity: "345.86845230",
+				},
+				{
+					Price:    "0.10490700",
+					Quantity: "0.00000000",
+				},
+			},
+		}
+		s.assertWsDepthEventEqual(e, event)
+	}, func(err error) {
+		s.r().EqualError(err, fakeErrMsg)
+	})
+	s.r().NoError(err)
+	stopC <- struct{}{}
+	<-doneC
+}
+
+func (s *websocketServiceTestSuite) TestDepth20FeatureServe() {
+	data := []byte(`{
+        "e": "depthUpdate",
+        "E": 1499404630606,
+        "s": "ETHBTC",
+        "u": 7913455,
+        "U": 7913452,
+        "b": [
+            [
+                "0.10376590",
+                "59.15767010",
+                []
+            ]
+        ],
+        "a": [
+            [
+                "0.10376586",
+                "159.15767010",
+                []
+            ],
+            [
+                "0.10383109",
+                "345.86845230",
+                []
+            ],
+            [
+                "0.10490700",
+                "0.00000000",
+                []
+            ]
+        ]
+    }`)
+	fakeErrMsg := "fake error"
+	s.mockWsServe(data, errors.New(fakeErrMsg))
+	defer s.assertWsServe()
+
+	doneC, stopC, err := WsDepth20FeatureServe("ETHBTC", func(event *WsDepthEvent) {
+		e := &WsDepthEvent{
+			Event:         "depthUpdate",
+			Time:          1499404630606,
+			Symbol:        "ETHBTC",
+			UpdateID:      7913455,
+			FirstUpdateID: 7913452,
+			Bids: []Bid{
+				{
+					Price:    "0.10376590",
+					Quantity: "59.15767010",
+				},
+			},
+			Asks: []Ask{
+				{
+					Price:    "0.10376586",
+					Quantity: "159.15767010",
+				},
+				{
+					Price:    "0.10383109",
+					Quantity: "345.86845230",
+				},
+				{
+					Price:    "0.10490700",
+					Quantity: "0.00000000",
+				},
+			},
+		}
+		s.assertWsDepthEventEqual(e, event)
+	}, func(err error) {
+		s.r().EqualError(err, fakeErrMsg)
+	})
+	s.r().NoError(err)
+	stopC <- struct{}{}
+	<-doneC
+}
+
+func (s *websocketServiceTestSuite) TestDepth20SpotServe() {
+	data := []byte(`{
+        "e": "depthUpdate",
+        "E": 1499404630606,
+        "s": "ETHBTC",
+        "u": 7913455,
+        "U": 7913452,
+        "b": [
+            [
+                "0.10376590",
+                "59.15767010",
+                []
+            ]
+        ],
+        "a": [
+            [
+                "0.10376586",
+                "159.15767010",
+                []
+            ],
+            [
+                "0.10383109",
+                "345.86845230",
+                []
+            ],
+            [
+                "0.10490700",
+                "0.00000000",
+                []
+            ]
+        ]
+    }`)
+	fakeErrMsg := "fake error"
+	s.mockWsServe(data, errors.New(fakeErrMsg))
+	defer s.assertWsServe()
+
+	doneC, stopC, err := WsDepth20SpotServe("ETHBTC", func(event *WsDepthEvent) {
+		e := &WsDepthEvent{
+			Event:         "depthUpdate",
+			Time:          1499404630606,
+			Symbol:        "ETHBTC",
+			UpdateID:      7913455,
+			FirstUpdateID: 7913452,
+			Bids: []Bid{
+				{
+					Price:    "0.10376590",
+					Quantity: "59.15767010",
+				},
+			},
+			Asks: []Ask{
+				{
+					Price:    "0.10376586",
+					Quantity: "159.15767010",
+				},
+				{
+					Price:    "0.10383109",
+					Quantity: "345.86845230",
+				},
+				{
+					Price:    "0.10490700",
+					Quantity: "0.00000000",
+				},
+			},
+		}
+		s.assertWsDepthEventEqual(e, event)
+	}, func(err error) {
+		s.r().EqualError(err, fakeErrMsg)
+	})
+	s.r().NoError(err)
+	stopC <- struct{}{}
+	<-doneC
+}
+
+func (s *websocketServiceTestSuite) TestDepthSpotServe() {
+	data := []byte(`{
+        "e": "depthUpdate",
+        "E": 1499404630606,
+        "s": "ETHBTC",
+        "u": 7913455,
+        "U": 7913452,
+        "b": [
+            [
+                "0.10376590",
+                "59.15767010",
+                []
+            ]
+        ],
+        "a": [
+            [
+                "0.10376586",
+                "159.15767010",
+                []
+            ],
+            [
+                "0.10383109",
+                "345.86845230",
+                []
+            ],
+            [
+                "0.10490700",
+                "0.00000000",
+                []
+            ]
+        ]
+    }`)
+	fakeErrMsg := "fake error"
+	s.mockWsServe(data, errors.New(fakeErrMsg))
+	defer s.assertWsServe()
+
+	doneC, stopC, err := WsDepthSpotServe("ETHBTC", func(event *WsDepthEvent) {
 		e := &WsDepthEvent{
 			Event:         "depthUpdate",
 			Time:          1499404630606,
@@ -289,7 +508,69 @@ func (s *websocketServiceTestSuite) TestKlineServe() {
 	s.mockWsServe(data, errors.New(fakeErrMsg))
 	defer s.assertWsServe()
 
-	doneC, stopC, err := WsKlineServe("ETHBTC", "1m", func(event *WsKlineEvent) {
+	doneC, stopC, err := WsKlineSpotServe("ETHBTC", "1m", func(event *WsKlineEvent) {
+		e := &WsKlineEvent{
+			Event:  "kline",
+			Time:   1499404907056,
+			Symbol: "ETHBTC",
+			Kline: WsKline{
+				StartTime:            1499404860000,
+				EndTime:              1499404919999,
+				Symbol:               "ETHBTC",
+				Interval:             "1m",
+				FirstTradeID:         77462,
+				LastTradeID:          77465,
+				Open:                 "0.10278577",
+				Close:                "0.10278645",
+				High:                 "0.10278712",
+				Low:                  "0.10278518",
+				Volume:               "17.47929838",
+				TradeNum:             4,
+				IsFinal:              false,
+				QuoteVolume:          "1.79662878",
+				ActiveBuyVolume:      "2.34879839",
+				ActiveBuyQuoteVolume: "0.24142166",
+			},
+		}
+		s.assertWsKlineEventEqual(e, event)
+	}, func(err error) {
+		s.r().EqualError(err, fakeErrMsg)
+	})
+	s.r().NoError(err)
+	stopC <- struct{}{}
+	<-doneC
+}
+
+func (s *websocketServiceTestSuite) TestKlineFeatureServe() {
+	data := []byte(`{
+        "e": "kline",
+        "E": 1499404907056,
+        "s": "ETHBTC",
+        "k": {
+            "t": 1499404860000,
+            "T": 1499404919999,
+            "s": "ETHBTC",
+            "i": "1m",
+            "f": 77462,
+            "L": 77465,
+            "o": "0.10278577",
+            "c": "0.10278645",
+            "h": "0.10278712",
+            "l": "0.10278518",
+            "v": "17.47929838",
+            "n": 4,
+            "x": false,
+            "q": "1.79662878",
+            "V": "2.34879839",
+            "Q": "0.24142166",
+            "B": "13279784.01349473"
+        }
+    }`)
+	fakeErrMsg := "fake error"
+	s.mockWsServe(data, errors.New(fakeErrMsg))
+	defer s.assertWsServe()
+
+	doneC, stopC, err := WsKlineFeatureServe("ETHBTC", "1m", func(event *WsKlineEvent) {
 		e := &WsKlineEvent{
 			Event:  "kline",
 			Time:   1499404907056,
